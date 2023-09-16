52c9489cc3ca23e2130361e4f5c510a43a078419
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2023-09-16 18:41:59 +0300

    Add create functionality

--- phonebook/app/controllers/contacts_controller.rb
+++ phonebook/app/controllers/contacts_controller.rb
@@ -6,4 +6,20 @@ class ContactsController < ApplicationController
   def show
     @contact = Contact.find(params[:id])
   end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.create(contact_params)
+
+    redirect_to contact_path(@contact)
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:firstname, :lastname, :phonenumber)
+  end
+
 end

91084d564eb6d1fb2cd78c2699ac3d05e81310a5
Sebastian Sergelius <sebastian_sergelius@hotmail.com>
2023-09-17 10:47:40 +0300

    Add phonenumber validation

--- phonebook/app/models/contact.rb
+++ phonebook/app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ApplicationRecord
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :phonenumber, format: { with: /\+\d+\s{1}\d+\s{1}\d{6,}/ }
   def full_name
     "#{firstname} #{lastname}"
   end
